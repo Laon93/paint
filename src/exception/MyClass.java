9744dd5e1761f111e71e70e7ea0d369cd0908239
YeonJin <duswls0515@gmail.com>
2017-08-03 14:23:50 +0900

    17.08.03 14:23 commit

--- src/exception/MyClass.java
+++ src/exception/MyClass.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class MyClass {
 
 	// throws Exception 해도 댐
-	public void danger() throws IOException {
+	// 저렇게 여러개 달아도 된당'ㅇ'
+	public void danger() throws IOException, MyException {
 		int i = 1;
 		System.out.println("some code ..." + i++);
 		System.out.println("some code ..." + i++);
--- src/exception/MyClassTest.java
+++ src/exception/MyClassTest.java
@@ -1,5 +1,16 @@
 package exception;
 
+import java.io.IOException;
+
 public class MyClassTest {
+	public static void main(String[] args) throws MyException {
+		MyClass myClass = new MyClass();
+		try {
+			myClass.danger();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
