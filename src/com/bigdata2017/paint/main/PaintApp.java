fca88e2011f320c3901595e3103902232b0ed9e4
YeonJin <duswls0515@gmail.com>
2017-08-07 11:05:03 +0900

    17.08.07 11:05

--- src/com/bigdata2017/paint/main/PaintApp.java
+++ src/com/bigdata2017/paint/main/PaintApp.java
@@ -22,11 +22,28 @@ public class PaintApp implements Resizeable {
 		// Shape
 		draw(new Triangle());
 		draw(new Rectangle());
+		
+		Point[] points = new Point[10];
+		
+		//anonymous class object
+		//딱 한번만 그 인터페이스에 구현되어있는 것을 사용하고 버리겠다 
+		//할때 사용 하는 것이 anonymous class object
+		//이름이 없는 익명 class
+		draw(new Drawable() {
+			@Override
+			public void draw() {
+				//결국 이름 없는 임시로 만든 class가 되는 것
+				System.out.println(points);
+				System.out.println("anonymous class 생성 완료");
+			}
+		});
+		
+		
 		// circle은 기본 생성자가 없으니까 생성자를 정의해둔 대로 생성을 해야함.
 		draw(new Circle(10, 20, 5));
-		
-		//resize test
-	//	resize(new Triangle());
+
+		// resize test
+		// resize(new Triangle());
 	}
 
 	/*
@@ -40,9 +57,9 @@ public class PaintApp implements Resizeable {
 
 	@Override
 	public void resize(double rate) {
-		
+
 		resize(0.5);
-		
+
 	}
 
 }
